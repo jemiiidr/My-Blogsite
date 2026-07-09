2ee2291d813530325c5a67597757da343ae3e54c
jemiiidr <jamiedelrosario321@gmail.com>
2026-07-09 12:03:43 +0800

    chore: Initialized project structure, packages, configurations.

---
+++ my-blogsite/drizzle.config.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+	throw new Error(
+		"DATABASE_URL is not defined. Check your .env configuration.",
+	);
+}
+
+export default defineConfig({
+	schema: "./db/schema.ts",
+	out: "./drizzle",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: databaseUrl,
+	},
+});
