0c26406ea8901adc8d96d016a05d07c6f79cdc9e
jemiiidr <jamiedelrosario321@gmail.com>
2026-07-10 10:41:00 +0800

    chore: Added lenis for smoothscroll

---
+++ my-blogsite/components/LenisProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ReactLenis } from "lenis/react";
+
+export default function LenisProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+
+return (
+    <ReactLenis
+        root
+        options={{
+        duration: 1.2,
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 2,
+        }}
+    >
+        {children}
+        
+    </ReactLenis>
+);
+
+}
